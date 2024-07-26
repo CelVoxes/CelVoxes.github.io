6d188c726be38dd18e5a70226aa27f023261de02
eonurk <onurkara3594@gmail.com>
2024-07-26 09:32:16 -0400

    unnecessary decleration

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import path from "path";
 import react from "@vitejs/plugin-react";
-import { ssr } from "vite-plugin-ssr/plugin";
+// import { ssr } from "vite-plugin-ssr/plugin";
 
 export default defineConfig({
 	plugins: [react()],
