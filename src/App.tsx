97ba8297f68f028f4623cac7c72cfed2b44e50c9
eonurk <onurkara3594@gmail.com>
2024-07-29 23:15:46 +0200

    basename removal from router

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
 	return (
 		<>
 			<Toaster />
-			<Router basename="">
+			<Router>
 				<Routes>
 					<Route path="/" element={<Home />} />
 					<Route path="/about" element={<About />} />
