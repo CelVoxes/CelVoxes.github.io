bae9ee5d70bd910b5c7b4a470fbf55a63bd534f7
eonurk <onurkara3594@gmail.com>
2024-08-07 14:57:46 +0200

    add h tag

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -16,10 +16,10 @@ export function Home() {
 			<Separator />
 			<div className="h-[40rem] flex justify-center items-center px-4">
 				<div className="text-2xl mx-auto font-normal text-neutral-600 dark:text-neutral-400">
-					<p>
+					<h1>
 						Introducing ceLLama, an automated cell type annotation pipeline
 						using local Large Language Models (LLMs).
-					</p>
+					</h1>
 					<div className="items-center flex justify-center">
 						<Link to="solutions/cellama">
 							<Button className="flex justify-center items-center px-16 py-6 mt-4">
